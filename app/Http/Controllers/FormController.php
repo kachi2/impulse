235ac69ca2d85b85247843b8edf63f94da8be67e
Michael Ozoudeh <jesmikky@gmail.com>
2025-04-10 23:40:50 +0100

    iniated new clone

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\FormCreationEmail;
+use App\Mail\SchoolFairMail;
+use App\Models\FormApplicants;
+use App\Models\SubMenu;
+use Barryvdh\DomPDF\Facade\Pdf;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Validator;
+
+class FormController extends Controller
+{
+    public function viewForm(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            'email' => 'required',
+            'name' => 'required',
+            'phone' => 'required',
+    ]);
+    if($validate->fails())
+    {
+        
+        return back()->withInput($request->all());
+    }
+        $chk = FormApplicants::where('email', $request->email)->first();
+        if(!$chk){
+         FormApplicants::create([
+            'email' => $request->email,
+            'name' => $request->name,
+            'phone' => $request->phone,
+            'date_signed' => Carbon::now(),
+            'parent_date_signed' => Carbon::now(),
+        ]);
+    }
+        $data['users'] =  FormApplicants::where('email', $request->email)->first();
+        Mail::to([$request->email, 'noreply@otegeeconcepts.com.ng'])->send(new SchoolFairMail(['name' => $request->name]));
+       return view('frontend.memphies',$data);
+    }
+
+    public function UpdateForm(Request $request)
+    {
+        $user = FormApplicants::where('email', $request->email)->first();
+        $validate = Validator::make($request->all(), [
+                'resume' => 'nullable',
+                'personal_statement' => 'nullable',
+                'parent_signature' => 'mimes:jpg,png,jpeg',
+                'student_signature' => ['nullable','mimes:jpg,png,jpeg'],
+        ]);
+        if($validate->fails())
+        {
+            Session::flash('message', $validate->errors()->first());
+            Session::flash('alert', 'danger');
+            $valid['breadcrums'] = SubMenu::where('id', 25)->first();
+            $valid['users'] = $user;
+              return view('frontend.memphies',$valid); 
+        }
+        $capt = captcha_check($request->captcha);
+        // if(!$capt){
+        //     Session::flash('message', 'Captcha does not match, try again');
+        //     Session::flash('alert', 'danger');
+        //     $valid['breadcrums'] = SubMenu::where('id', 25)->first();
+        //     $valid['users'] = $user; 
+        //  return view('frontend.memphies',$valid);
+           
+        // }
+          if($request->document){
+            $image = $request->file('document');
+            $fileName = $image->getClientOriginalName();
+            $ext = $image->getClientOriginalExtension();
+            $consentForm =  $fileName;
+            $image->move('images',$fileName);
+        }
+        if($request->parent_signature){
+            $image = $request->file('parent_signature');
+            $fileName = $image->getClientOriginalName();
+            $ext = $image->getClientOriginalExtension();
+            $parent_signature = $fileName;
+            $image->move('images',$parent_signature);
+        }
+        if($request->student_signature){
+            $image = $request->file('student_signature');
+            $fileName = $image->getClientOriginalName();
+            // $ext = $image->getClientOriginalExtension();
+            $student_signature = $fileName;
+            $image->move('images',$student_signature);
+        }
+        if($request->personal_statement){
+            $image = $request->file('personal_statement');
+            $fileName = $image->getClientOriginalName();
+            $ext = $image->getClientOriginalExtension();
+            $personal_statement = time().'.'.$ext;
+            $image->move('images',$personal_statement);
+        }
+        if($request->resume){
+            $image = $request->file('resume');
+            $fileName = $image->getClientOriginalName();
+            $ext = $image->getClientOriginalExtension();
+            $resume = $fileName;
+            $image->move('images',$resume);
+        }
+        $data = [
+            'student_signature' => $student_signature??'',
+            'parent_name' => $request->parent_name,
+            'parent_signature' => $parent_signature??null,
+            'document' => $consentForm??null,
+            'resume' => $resume??null,
+            'personal_statement' => $personal_statement??null,
+        ];
+
+        
+        $user->update($data);
+        $data['name'] = $user['name'];
+        $data['email'] = $user['email'];
+        $data['phone'] = $user['phone'];
+        Mail::to('college-fair@otegeeconcepts.com.ng')->send(new FormCreationEmail($data));
+        Session::flash('alert', 'success');
+        Session::flash('message', "Registration completed successfully");
+        $data['breadcrums'] = SubMenu::where('id', 25)->first();
+        $data['users'] =$user;
+     return view('frontend.memphies',$data);
+    }
+}
---
+++ resources/views/emails/schoolFair.blade.php
@@ -0,0 +1,52 @@
+<div width="100%" style="margin:0;padding:0!important;background-color:#f5f6fa">
+    <span style="width:100%;background-color:#f5f6fa">
+        <table width="100%" border="0" cellpadding="0" cellspacing="0" bgcolor="#f5f6fa">
+            <tbody> 
+                <tr>
+                    <td style="padding:30px 0">
+                        <table style="width:100%;max-width:620px;margin:0 auto">
+                            <tbody>
+                                <tr>
+                                    <td style="text-align:center;padding-bottom:15px">
+                                        <img style="max-height:50px;width:auto"
+                                            src="{{ asset('assets/' . $settings->logo) }}"
+                                            alt="{{ $settings->site_name }}" class="CToWUd">
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <table style="width:100%;max-width:620px;margin:0 auto;background-color:#ffffff">
+                            <tbody>
+                                <tr>
+                                    <td style="padding:10px 30px 10px">
+                                       <p> Hello {{$data['name']}},</p>  
+                                      <p>Thank you for expressing your interest in studying at the University of Memphis. </p> 
+
+                                      <p> We will send you an invitation to our Zoom Knowledge Sharing Session shortly. Kindly check your email and spam folder for our email.</p> 
+                                       
+                                      <p>We look forward to seeing you at the meeting. </p> 
+                                       
+                                       <p>Warm regards, </p> 
+                                       <p> Otegee Concepts </p>
+                                    </td>
+                                <tr>
+                                    <td style="padding:0 30px">
+                                        <table style="width:100%;max-width:620px;margin:0 auto">
+                                            <tbody>
+                                                <tr>
+                                                    <td style="text-align:center;padding:25px 20px 0">
+                                                        <p style="font-size:13px">{{ $settings->site_name }} ©
+                                                            {{ Date('Y') }}.</p>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+    </span>
+    <div class="yj6qo"></div>
+    <div class="adL">
+    </div>
+</div>
